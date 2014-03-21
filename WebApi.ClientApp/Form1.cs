860a43d8b525fc57e7c8804df6f882efa7d11315
kimx <kimxinfo@gmail.com>
2014-03-21 17:21:45 +0800

    first

---
+++ WebApi.ClientApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebApiLab.Models;
+
+namespace WebApi.ClientApp
+{
+    //*package:Microsoft.AspNet.WebApi.Client
+    public partial class Form1 : Form
+    {
+        HttpClient client = new HttpClient();
+        public Form1()
+        {
+            client.BaseAddress = new Uri("http://localhost:3752");
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetAll();
+        }
+
+        private void GetAll()
+        {
+            HttpResponseMessage resp = client.GetAsync("api/products").Result;
+            resp.EnsureSuccessStatusCode();
+            var products = resp.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            MessageBox.Show(products.Count().ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var resp = client.GetAsync(string.Format("api/products/{0}", 1)).Result;
+            resp.EnsureSuccessStatusCode();
+            var product = resp.Content.ReadAsAsync<Product>().Result;
+            MessageBox.Show(product.Name);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string query = string.Format("api/products?category={0}", "TAIFEI");
+            var resp = client.GetAsync(query).Result;
+            resp.EnsureSuccessStatusCode();
+            var products = resp.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            MessageBox.Show(products.Count().ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            p.Name = "Post Name";
+            p.Id = 999;
+            p.Price = 100;
+            var resp = client.PostAsJsonAsync("api/products", p).Result;
+            resp.EnsureSuccessStatusCode();
+            GetAll();
+        }
+
+
+    }
+}
---
+++ WebApiLab/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiLab.Models;
+
+namespace WebApiLab.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static Product[] products = new Product[]  
+        {  
+            new Product { Id = 1, Name = "Coffee", Category = "TAIFEI", Price = 1 },  
+            new Product { Id = 2, Name = "Bean", Category = "TAIFEI", Price = 3.75M },  
+            new Product { Id = 3, Name = "Ship", Category = "MPISI", Price = 16.99M }  
+        };
+
+        //Product
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        //Product/1
+        public Product GetProductById(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return product;
+        }
+
+        //Product/?category=TAIFEI
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return products.Where(p => string.Equals(p.Category, category,
+                    StringComparison.OrdinalIgnoreCase));
+        }
+
+        // POST api/products
+        public void Post([FromBody]Product value)
+        {
+            var list = products.ToList();
+            list.Add(value);
+            products = list.ToArray();
+        }
+
+
+    }
+}
